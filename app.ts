bd9a5227d2377bc00c5664d84283acbf81e983dd
Dereje <getutadessedereje@icloud.com>
2022-06-16 18:56:23 +0200

    Update : ajout class

--- app.ts
+++ app.ts
@@ -1,3 +1,22 @@
 const allBooks = document.querySelectorAll('.book-list') as NodeListOf<HTMLTableElement>;
 const form = document.querySelector('.book') as HTMLFormElement;
 
+class Book {
+    constructor( public titre: string, public auteur: string, public annee: number){
+        this.titre = titre;
+        this.auteur = auteur;
+        this.annee = annee;
+    }
+
+    addBookTolist(book : Book){
+        const bookList = document.querySelector('.book-list') as HTMLTableElement;
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+            <td>${book.titre}</td>
+            <td>${book.auteur}</td>
+            <td>${book.annee}</td>
+            td><td><button class="delete">X</button></td>
+        `;
+    }
+
+}
--- build/app.js
+++ build/app.js
@@ -1,3 +1,23 @@
 "use strict";
 const allBooks = document.querySelectorAll('.book-list');
 const form = document.querySelector('.book');
+class Book {
+    constructor(titre, auteur, annee) {
+        this.titre = titre;
+        this.auteur = auteur;
+        this.annee = annee;
+        this.titre = titre;
+        this.auteur = auteur;
+        this.annee = annee;
+    }
+    addBookTolist(book) {
+        const bookList = document.querySelector('.book-list');
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+            <td>${book.titre}</td>
+            <td>${book.auteur}</td>
+            <td>${book.annee}</td>
+            td><td><button class="delete">X</button></td>
+        `;
+    }
+}
